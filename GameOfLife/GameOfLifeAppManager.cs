ef0c7e2aad56193bea6bb7774b249109349bfa6f
Garvit <sharmagarvit614@gmail.com>
2024-04-13 22:04:12 +0530

    checking if i fixed my git user

--- GameOfLife/GameOfLifeAppManager.cs
+++ GameOfLife/GameOfLifeAppManager.cs
@@ -9,10 +9,13 @@ public class GameOfLifeAppManager : IPixelWindowAppManager
     public const int PixelScale = 1;
     public const int WindowWidth = 800;
     public const int WindowHeight = 450;
+    
     public const int Width = WindowWidth / PixelScale;
     public const int Height = WindowHeight / PixelScale;
+    
     public const uint EditingFramerate = 500;
     public const uint GameFramerate = 500;
+    
     private static readonly (byte, byte, byte) Dead = (0, 0, 0);
     private static readonly (byte, byte, byte) Alive = (255, 255, 255);
     
@@ -23,6 +26,7 @@ public class GameOfLifeAppManager : IPixelWindowAppManager
     private bool _editing = true;
     private bool _leftMouseButtonPressed;
     private bool _rightMouseButtonPressed;
+    
     public void OnLoad(RenderWindow renderWindow)
     {
         _window = renderWindow;
@@ -34,6 +38,7 @@ public class GameOfLifeAppManager : IPixelWindowAppManager
             if (args.Code is Keyboard.Key.Enter) _editing = !_editing;
             _window.SetFramerateLimit(_editing ? EditingFramerate : GameFramerate);
         };
+        
         _window.MouseButtonPressed += (_, args) =>
         {
             switch (args.Button)
@@ -46,6 +51,7 @@ public class GameOfLifeAppManager : IPixelWindowAppManager
                     break;
             }
         };
+        
         _window.MouseButtonReleased += (_, args) =>
         {
             switch (args.Button)
@@ -58,6 +64,7 @@ public class GameOfLifeAppManager : IPixelWindowAppManager
                     break;
             }
         };
+        
         _window.MouseMoved += (_, args) =>
         {
             if (!_editing || args.X > WindowWidth || args.Y > WindowHeight) return;
@@ -73,34 +80,35 @@ public class GameOfLifeAppManager : IPixelWindowAppManager
     public void Update(float frameTime)
     {
         if (_editing) return;
+        
         for (uint i = 0; i < Height; i++)
+        for (uint j = 0; j < Width; j++)
         {
-            for (uint j = 0; j < Width; j++)
+            var neighbours = new[]
             {
-                var neighbours = new[]
-                {
-                    i >= 1 ? _old[j, i - 1] : Dead, // up
-                    i + 1 < Height ? _old[j, i + 1] : Dead, // down
-                    j + 1 < Width ? _old[j + 1, i] : Dead, // right
-                    j >= 1 ? _old[j - 1, i] : Dead, // left
+                i >= 1 ? _old[j, i - 1] : Dead, // up
+                i + 1 < Height ? _old[j, i + 1] : Dead, // down
+                j + 1 < Width ? _old[j + 1, i] : Dead, // right
+                j >= 1 ? _old[j - 1, i] : Dead, // left
 
-                    j >= 1 && i >= 1 ? _old[j - 1, i - 1] : Dead, // upLeft
-                    j + 1 < Width && i >= 1 ? _old[j + 1, i - 1] : Dead, // upRight
-                    j >= 1 && i + 1 < Height ? _old[j - 1, i + 1] : Dead, // downLeft
-                    j + 1 < Width && i + 1 < Height ? _old[j + 1, i + 1] : Dead, // downRight
-                };
-                var aliveNeighbours = neighbours.Count(neighbour => neighbour.Item1 == 255);
+                j >= 1 && i >= 1 ? _old[j - 1, i - 1] : Dead, // upLeft
+                j + 1 < Width && i >= 1 ? _old[j + 1, i - 1] : Dead, // upRight
+                j >= 1 && i + 1 < Height ? _old[j - 1, i + 1] : Dead, // downLeft
+                j + 1 < Width && i + 1 < Height ? _old[j + 1, i + 1] : Dead, // downRight
+            };
 
-                var currentState = _old[j, i];
-                var newState = aliveNeighbours switch
-                {
-                    < 2 => Dead,
-                    2 => currentState,
-                    <= 3 => Alive,
-                    > 3 => Dead,
-                };
-                _new[j, i] = newState;
-            }
+            var aliveNeighbours = neighbours.Count(neighbour => neighbour.Item1 == 255);
+
+            var currentState = _old[j, i];
+            var newState = aliveNeighbours switch
+            {
+                < 2 => Dead,
+                2 => currentState,
+                <= 3 => Alive,
+                > 3 => Dead,
+            };
+
+            _new[j, i] = newState;
         }
     }
 
